8f2ab3cbc6d04c65fece1a495d7168a06526ffab
Kurban Akhmedov <malkolm.63.zed@mail.ru>
2022-08-10 23:52:57 +0300

    дополнил код тем что с профиля вк автоматически будет загружать на яндекс диск

--- kursovaya.py
+++ kursovaya.py
@@ -1,47 +1,81 @@
 import requests
 from pprint import pprint
 
-####  ПРОГРАММА ДЛЯ ПРОСМОТРА ВСЕХ ФОТОГРАФИЙ(АВАТАРОК) ПОЛЬЗОВАТЕЛЕЙ С ВКОНТАКТЕ + её определенные данные       #######
-##########   СОХРАНЯЕМ ОТДЕЛЬНО В JSON ФОРМАТЕ В ЗАДАННОЙ ПЕРЕМЕННОЙ      ##############################################
+vk_user_id = 1
+with open('token_VK.txt', 'r') as file_object:
+    token_VK = file_object.read().strip()
 
-vk_user_id = 184465493  # id пользователя, у которого будут читаться аватарки
-with open('token_VK.txt', 'r') as file_object:#читаем токен с вк для дальнейшей работы
-    token = file_object.read().strip()
+with open('token_YA.txt', 'r') as file_object:
+    token_YA = file_object.read().strip()
 
 
-def get_foto_data(offset=0, count=10):# в первом аргументе задана по умолчания время на итерацию, во втором:
-                                                                            # колличество значений для чтения
-    api = requests.get("https://api.vk.com/method/photos.get", # url c методом чтения фотографий
-                       params={
-                           'owner_id': vk_user_id, # ввели нашего пользователя
-                           'access_token': token, #токен для аутентификации
-                           'offset': offset,
-                           'count': count,
-                           'photo_size': 0,
-                           'extended': 1, # 1 - в апи вк значит дополнительные данные по фото в ключе "extended"
-                           'v': 5.131,
-                           'album_id': 'profile', # выбираем значение "profile" - работа с аватарками
+class VK_and_YA_upload_files:
+    def __init__(self, token_vk, token_ya, vk_user_id):
 
-                       })
-    photos_info = [] # список в который будем добавлять словари с данными по фото
-    for data in api.json()['response']['items']:#обращаемся в json конкретно к определенным значениям, которые нам нужны
-                                                        #а именно:
+        self.token_vk = token_vk
+        self.token_ya = token_ya
+        self.photos_info = []
+        self.vk_user_id = vk_user_id
 
-        photo_info = {
-            'file_name': f"{data['likes']['count']}.png", # добавляя файл с фото мы именуем его с кол-ом лайков, которое набрало фото
-            'url': data['sizes'][-1]['url'], #в текущий словарь добавляем конкретный максимальный размер из выбранного
-            'size': data['sizes'][-1]['type'] #сохраняем данные с выбранным размером
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token_ya}'
         }
 
-        for photo in photos_info: #этот цикл переименовывает фото, если вдруг колличество лайков совпадало и разных фото
-            # что бы не было повторений в наименованиях, переименуем фото с повторяющимся названием,на название
-            #                                                                                даты её добавления
-            if photo['file_name'] == f"{data['likes']['count']}.png":
-                photo_info['file_name'] = f"{data['date']}.png"
+    def get_foto_data(self):
+        api = requests.get("https://api.vk.com/method/photos.get",
+                           params={
+                               'owner_id': self.vk_user_id,
+                               'access_token': self.token_vk,
+                               'offset': 0,
+                               'count': 2,
+                               'photo_size': 0,
+                               'extended': 1,
+                               'v': 5.131,
+                               'album_id': 'profile',
 
-        photos_info.append(photo_info)
+                           })
 
-    pprint(photos_info)
+        for data in api.json()['response']['items']:
+
+            photo_info = {
+                'file_name': f"{data['likes']['count']}.png",
+                'url': data['sizes'][-1]['url'],
+                'size': data['sizes'][-1]['type']
+            }
+
+            for photo in self.photos_info:
+                if photo['file_name'] == f"{data['likes']['count']}.png":
+                    photo_info['file_name'] = f"{data['date']}.png"
+
+            self.photos_info.append(photo_info)
+        # pprint(self.photos_info)
+
+    def folder(self):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources"
+        self.folder = 'vk'
+        params = {'path': f'/{self.folder}'}
+        headers = self.get_headers()
+        response = requests.put(upload_url, headers=headers, params=params)
+        if response.status_code == 201:
+            print(f'Папка {self.folder} успешно создана на яндекс.диске.')
+        elif response.status_code == 409:
+            print(f'Папка {self.folder} уже существует на яндекс.диске.')
+
+    def downloads_photos(self, url, path):
+
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        params = {'url': url, 'path': path}
+        headers = self.get_headers()
+        response = requests.post(upload_url, headers=headers, params=params)
+        print(response.status_code)
 
 
-get_foto_data()
+vk = VK_and_YA_upload_files(token_VK, token_YA, 1)
+vk.folder()
+vk.get_foto_data()
+
+# vk.new_download()
+for i in vk.photos_info:
+    vk.downloads_photos(i['url'], f"vk/{i['file_name']}")
