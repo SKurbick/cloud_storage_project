849dfd247ac6c360daf9676b6ce6f7a6fa2842b8
Kurban Akhmedov <malkolm.63.zed@mail.ru>
2022-08-14 14:22:34 +0300

    итоговая версия курсовой

--- kursovaya_2.0.py
+++ kursovaya_2.0.py
@@ -1,8 +1,7 @@
 import requests
-from pprint import pprint
 import json
+from progress.bar import IncrementalBar
 
-vk_user_id = 1
 with open('token_VK.txt', 'r') as file_object:
     token_VK = file_object.read().strip()
 
@@ -10,27 +9,20 @@ with open('token_YA.txt', 'r') as file_object:
     token_YA = file_object.read().strip()
 
 
-class VK_and_YA_upload_files:
-    def __init__(self, token_vk, token_ya, vk_user_id):
-
+class VkProfileUpload:
+    def __init__(self, token_vk, user_id, count):
         self.token_vk = token_vk
-        self.token_ya = token_ya
+        self.user_id = user_id
         self.photos_info = []
-        self.vk_user_id = vk_user_id
-
-    def get_headers(self):
-        return {
-            'Content-Type': 'application/json',
-            'Authorization': f'OAuth {self.token_ya}'
-        }
+        self.count = count
 
     def get_foto_data(self):
         api = requests.get("https://api.vk.com/method/photos.get",
                            params={
-                               'owner_id': self.vk_user_id,
+                               'owner_id': self.user_id,
                                'access_token': self.token_vk,
                                'offset': 0,
-                               'count': 2,
+                               'count': self.count,
                                'photo_size': 0,
                                'extended': 1,
                                'v': 5.131,
@@ -52,9 +44,20 @@ class VK_and_YA_upload_files:
 
             self.photos_info.append(photo_info)
 
+
+class YaDiskUpload:
+    def __init__(self, folder, token_ya):
+        self.folder = folder
+        self.token_ya = token_ya
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token_ya}'
+        }
+
     def add_folder(self):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources"
-        self.folder = 'vk'
         params = {'path': f'/{self.folder}'}
         headers = self.get_headers()
         response = requests.put(upload_url, headers=headers, params=params)
@@ -68,16 +71,24 @@ class VK_and_YA_upload_files:
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         params = {'url': url, 'path': path}
         headers = self.get_headers()
-        response = requests.post(upload_url, headers=headers, params=params)
-        print(response.status_code)
+        requests.post(upload_url, headers=headers, params=params)
 
 
-vk = VK_and_YA_upload_files(token_VK, token_YA, 1)
-# vk.add_folder()
+user_id_input = input('введите id пользователя')
+photos_count = input('введите колличество фото для загрузки:')
+
+vk = VkProfileUpload(token_VK, user_id_input, photos_count)
+ya = YaDiskUpload('vk', token_YA)
 vk.get_foto_data()
-# pprint((vk.photos_info))
-# for i in vk.photos_info:
-#     vk.downloads_photos(i['url'], f"vk/{i['file_name']}")
+ya.add_folder()
+
+bar = IncrementalBar('photo', max=len(vk.photos_info))
+for el in vk.photos_info:
+    bar.next()
+    ya.downloads_photos(el['url'], f"vk/{el['file_name']}")
+bar.finish()
+print("Фотографии успешно загружены")
+
 json_file = []
 for js in vk.photos_info:
     b = js.pop("url")
@@ -85,4 +96,3 @@ for js in vk.photos_info:
 
 with open('json.json', 'w') as jsf:
     json.dump(json_file, jsf)
-
